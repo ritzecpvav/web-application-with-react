0cd80bdd945fff99311a3f7e82f1f6f5bf966191
Ritz Ghever Libetario <yahshua.ecpvav@gmail.com>
2021-10-05 03:51:25 +0000

    Building Application using React

---
+++ src/api/courseApi.js
@@ -0,0 +1,39 @@
+import { handleResponse, handleError } from "./apiUtils";
+const baseUrl = process.env.REACT_APP_API_URL + "/courses/";
+
+export function getCourses() {
+  return fetch(baseUrl)
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function getCourseBySlug(slug) {
+  return fetch(baseUrl + "?slug=" + slug)
+    .then(response => {
+      if (!response.ok) throw new Error("Network response was not ok.");
+      return response.json().then(courses => {
+        if (courses.length !== 1) throw new Error("Course not found: " + slug);
+        return courses[0];
+      });
+    })
+    .catch(handleError);
+}
+
+export function saveCourse(course) {
+  return fetch(baseUrl + (course.id || ""), {
+    method: course.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      ...course,
+      authorId: parseInt(course.authorId, 10)
+    })
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function deleteCourse(courseId) {
+  return fetch(baseUrl + courseId, { method: "DELETE" })
+    .then(handleResponse)
+    .catch(handleError);
+}
